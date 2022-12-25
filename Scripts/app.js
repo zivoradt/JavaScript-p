43006679ef8087fc5226be2f539b3e7ff612a933
zivoradt <zivoradtrickovic@gmail.com>
2022-12-25 22:32:50 +0100

    added IIFe to app.js

---
+++ Scripts/app.js
@@ -0,0 +1,16 @@
+ /* Custom JS goes here */
+
+ // IIFE - Imediately Invoked Funktion Expression
+ 
+ (function(){
+
+
+    function Start()
+    {
+        console.log("App started!"); 
+    }
+
+    window.onload = Start;
+
+
+ })();
