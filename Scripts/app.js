a7074ddb0fe96093b1601249d0973afe1ed382a4
zivoradt <zivoradtrickovic@gmail.com>
2022-12-27 12:55:19 +0100

    updated project

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,33 +1,68 @@
- /* Custom JS goes here */
+/* Custom JS goes here */
 
- // IIFE - Imediately Invoked Funktion Expression
- 
- (function(){
+// IIFE - Imediately Invoked Funktion Expression
+
+(function () {
+    "use strict"
+
+    // About event hendler
+    function AboutButtonClick() {
+        console.log("About Button clicked");
+    }
+    function AboutButtonOver(event) {
+        event.currentTarget.style.opacity = 0.3;
+    }
+
+    function AboutButtonOut(event) {
+        event.currentTarget.style.opacity = 1.0;
+    }
+
+    function HomeContent() {
+        let AboutButton = document.getElementById("AboutButton");
+        AboutButton.innerText = "About Us";
+
+        //About Button event listener
+        AboutButton.addEventListener("click", AboutButtonClick);
+        AboutButton.addEventListener("onmousover", AboutButtonOver);
+        AboutButton.addEventListener("onmouseout", AboutButtonOut);
+    }
+
+    function ReturnPi() {
+        return Math.PI;
+    }
+
+    function AboutPage() {
+        let number = ReturnPi();
+        console.log("This is a PI: " + number);
+
+        let paragraph = document.getElementById("paragraph");
+
+        let sentence = "We will chnage content";
+
+        paragraph.textContent = sentence;
+
+    }
 
 
-    function Start()
-    {
-        //local variable
+    function Start() {
+
         let title = document.title;
+
+        console.log("The page name is: " + title);
 
         switch (title) {
             case "Home":
-                let AboutButton = document.getElementById("AboutButton");
-
-        AboutButton.addEventListener("click", function(){
-            console.log("ABout Button was clicked");
-        });
-                
+                HomeContent();
                 break;
-        
+
+            case "About":
+                AboutPage();
             default:
                 break;
-        }
-
-        console.log("Title: " + title); 
+        };
     }
 
-    window.onload = Start;
+    window.addEventListener("load", Start);
 
 
- })();
+})();
