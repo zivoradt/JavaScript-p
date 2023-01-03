f563e5568e85cce42d098af1728e872af0fbabc0
zivoradt <zivoradtrickovic@gmail.com>
2023-01-03 11:43:05 +0100

    separated files in JS

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,93 +1,9 @@
-/* Custom JS goes here */
-
 // IIFE - Imediately Invoked Funktion Expression
 
-let app = (function () {
+let app;
+(function (app) {
     "use strict"
 
-
-
-    // About event hendler
-    function AboutButtonClick() {
-        console.log("About Button clicked");
-    }
-    function AboutButtonOver(event) {
-        event.currentTarget.style.opacity = 0.3;
-    }
-
-    function AboutButtonOut(event) {
-        event.currentTarget.style.opacity = 1.0;
-    }
-
-    function HomeContent() {
-        let AboutButton = document.getElementById("AboutButton");
-        AboutButton.innerText = "About Us";
-
-        //About Button event listener
-        AboutButton.addEventListener("click", AboutButtonClick);
-        AboutButton.addEventListener("onmousover", AboutButtonOver);
-        AboutButton.addEventListener("onmouseout", AboutButtonOut);
-    }
-
-    function ReturnPi() {
-        return Math.PI;
-    }
-
-    function AboutPage() {
-        
-
-        let paragraph = document.getElementById("paragraph");
-
-        let sentence = "We will chnage content";
-
-        paragraph.textContent = sentence;
-
-        let myArray = [
-            { name: "Tom", age: 25 },
-            { name: "zom", age: 26 },
-            { name: "Tomm", age: 4 },
-            { name: "Toom", age: 25 },
-        ];
-
-        myArray.push({ name: "TTTToom", age: 2522 });
-        myArray.unshift({ name: "Zile ", age: 25 });
-
-       
-
-        let myFavoriteThingList = [
-            "Video Games",
-            "Movies",
-            "Cars",
-            "VR"
-        ]
-
-        // hook into a ul
-        let myFavoriteList = document.getElementById("myFavorite");
-
-        myFavoriteThingList.forEach(element => {
-            let newItem = document.createElement("li");
-            newItem.textContent = element;
-            myFavoriteList.appendChild(newItem);
-
-        });
-
-
-
-
-        let arrayEmpty = (myArray.length > 0) ? false : true;
-
-        let myAssociate = [];
-
-        myAssociate["Name"] = "tom";
-        myAssociate["age"] = 30;
-        myAssociate["student"] = "P3034";
-
-        console.log(myAssociate);
-
-        
-
-
-    }
 
     function ContactContent() {
 
@@ -104,11 +20,11 @@ let app = (function () {
           
         switch (title) {
             case "Home":
-                HomeContent();
+                content.HomeContent();
                 break;
 
             case "About":
-                AboutPage();
+                content.AboutContent();
                 break;
 
             case "Contact":
@@ -128,7 +44,8 @@ let app = (function () {
 
     window.addEventListener("load", Start);
 
-    return {
-        title: document.title
-    };
-})();
+   app.Title = document.title;
+   
+   app.Img = document.getElementsByTagName("img")[0];
+
+})(app || (app = {}));
---
+++ Scripts/home.js
@@ -0,0 +1,28 @@
+let content;
+(function (content){
+
+    function AboutButtonClick() {
+        console.log("About Button clicked");
+    }
+    function AboutButtonOver(event) {
+        event.currentTarget.style.opacity = 0.3;
+    }
+
+    function AboutButtonOut(event) {
+        event.currentTarget.style.opacity = 1.0;
+    }    
+
+
+    function HomeContent() {
+        let AboutButton = document.getElementById("AboutButton");
+        AboutButton.innerText = "About Us";
+
+        //About Button event listener
+        AboutButton.addEventListener("click", AboutButtonClick);
+        AboutButton.addEventListener("onmousover", AboutButtonOver);
+        AboutButton.addEventListener("onmouseout", AboutButtonOut);
+    }
+    // attach Homecontent function to the content namespace
+    content.HomeContent = HomeContent;
+
+})(content || (content = {}))
