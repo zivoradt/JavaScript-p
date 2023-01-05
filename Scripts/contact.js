e40bb21c90c7cde389c7d1316c09880090ad0de2
zivoradt <zivoradtrickovic@gmail.com>
2023-01-05 20:10:48 +0100

    form validation is added

--- Scripts/contact.js
+++ Scripts/contact.js
@@ -1,11 +1,14 @@
 ((content) => {
 
-    function outputFormData(){
-         let name = document.getElementById("FullName");
-         let email = document.getElementById("EmailAdress");
-         let message = document.getElementById("Message");
-         let contact = document.getElementById("ContactNumber");
-        
+    // Local variables
+
+        let name = document.getElementById("FullName");
+        let email = document.getElementById("EmailAdress");
+        let message = document.getElementById("Message");
+        let contact = document.getElementById("ContactNumber");
+
+    function outputFormData() {
+
         console.log(`Full name: ${name.value}`);
         console.log(`Contact number: ${contact.value}`);
         console.log(`Email: ${email.value}`);
@@ -13,21 +16,80 @@
         console.log(`Form lenght: ${document.forms[0].length}`);
         console.log(`Form elements: ${document.forms[0].elements[0]}`);
 
-
         for (let index = 0; index < document.forms[0].length; index++) {
             console.log(`%c Element ${index}: ${document.forms[0].elements[index].value}`);
         }
-            
+    }
+
+    function clearValidation(){
+
+        name.setCustomValidity(""); 
+        email.setCustomValidity("");
+        message.setCustomValidity("");
+        contact.setCustomValidity("");
+    }
+
+   
+
+    function setEventForElements(){
         
+        for (const element of document.forms[0].elements) {
+            if ((element.tagName === "INPUT") || (element.tagName === "TEXTAREA")) {
+                //When user input data
+                element.addEventListener("input", function(){
+                    element.setCustomValidity("");
+                });
 
-    };
- 
+                //When user enter incorrect data
+                element.addEventListener("invalid", function(){
+                    switch(element.id) {
+                        case "FullName":
+                            element.setCustomValidity("You must enter an appropriate full name");
+                            break;
+                        case "EmailAdress":
+                            element.setCustomValidity("You must enter an appropriate email adress");
+                            break;
+                        case "Message":
+                            element.setCustomValidity("You must enter an appropriate message");
+                            break;
+                        case "ContactNumber":
+                            element.setCustomValidity("You must enter an appropriate contact number");
+                            break;
+                        default:
+                            element.setCustomValidity("This Field is requred");
+                    }
+                })
+            }
+            
+        }
+    }
 
+    function validateForm() {
+        setEventForElements();
 
+    }
+
+    function checkRequired(inputArr){
+        inputArr.forEach(input => {
+            if (input.value.trim() === '') {
+                showError(input, `${getFildName(input)} is Requred`);
+            }
+            console.log(input.value);
+
+        });
+    }
+
+    function getFildName(input){
+        return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+    }
+
+    let form  = document.form()
 
 
 
     function ContactContent() {
+
+       // clearValidation();
         
 
         document.getElementsByClassName("card-title")[0].textContent = "Contact You!";
@@ -40,21 +102,27 @@
         cancelButton.textContent = "Cancel";
         cancelButton.addEventListener("click", function (event) {
             event.preventDefault();
-             window.open("index.html", "_parent");
+            window.open("index.html", "_parent");
         });
 
         // add the HTML element  to page
         document.forms[0].appendChild(cancelButton);
 
         let SubmitButton = document.getElementById("sendButton");
-        SubmitButton.addEventListener("click", (event)=>{
-                event.preventDefault();
+        SubmitButton.addEventListener("click", (event) => {
+            // event.preventDefault();
+
+            if (!document.forms[0].checkValidity()) {
                 outputFormData();
+                validateForm();
+            }
+
         });
 
 
     }
 
+    // Public functions exposed to the content namespace
     content.ContactContent = ContactContent;
 
 
